c6a1b5552f53ed00158de7fc77d8ae7ed0fed806
Cactus <roccoagiacomino@gmail.com>
2025-05-25 11:23:10 -0500

    Create themes.lua

---
+++ themes.lua
@@ -0,0 +1,73 @@
+Themes = {
+	dark = {
+		Background = Color3.fromRGB(32, 32, 32),
+		Accent = Color3.fromRGB(0, 153, 255),
+		Foreground = Color3.fromRGB(45, 45, 45),
+		Text = Color3.fromRGB(255, 255, 255),
+		Hover = Color3.fromRGB(70, 70, 70)
+	},
+	impulse = {
+		Background = Color3.fromRGB(30,30,30),
+		Accent = Color3.fromRGB(43, 141, 86),         
+		Foreground = Color3.fromRGB(45, 45, 45),   
+		Text = Color3.fromRGB(76, 245, 152),
+		Hover = Color3.fromRGB(56, 200, 120)
+	},
+  LightTheme = {
+    Background = Color3.fromRGB(255, 255, 255),
+    Accent = Color3.fromRGB(150, 150, 150),
+    Foreground = Color3.fromRGB(200, 200, 200),
+    Text = Color3.fromRGB(0, 0, 0),
+    Hover = Color3.fromRGB(224, 224, 224)
+  },
+  BloodTheme = {
+      Background = Color3.fromRGB(10, 10, 10),
+      Accent = Color3.fromRGB(227, 27, 27),
+      Foreground = Color3.fromRGB(5, 5, 5),
+      Text = Color3.fromRGB(255, 255, 255),
+      Hover = Color3.fromRGB(20, 20, 20)
+  },
+  GrapeTheme = {
+      Background = Color3.fromRGB(64, 50, 71),
+      Accent = Color3.fromRGB(166, 71, 214),
+      Foreground = Color3.fromRGB(36, 28, 41),
+      Text = Color3.fromRGB(255, 255, 255),
+      Hover = Color3.fromRGB(74, 58, 84)
+  },
+  Ocean = {
+      Background = Color3.fromRGB(26, 32, 58),
+      Accent = Color3.fromRGB(86, 76, 251),
+      Foreground = Color3.fromRGB(38, 45, 71),
+      Text = Color3.fromRGB(200, 200, 200),
+      Hover = Color3.fromRGB(38, 45, 71)
+  },
+  Midnight = {
+      Background = Color3.fromRGB(44, 62, 82),
+      Accent = Color3.fromRGB(26, 189, 158),
+      Foreground = Color3.fromRGB(57, 81, 105),
+      Text = Color3.fromRGB(255, 255, 255),
+      Hover = Color3.fromRGB(52, 74, 95)
+  },
+  Sentinel = {
+      Background = Color3.fromRGB(32, 32, 32),
+      Accent = Color3.fromRGB(230, 35, 69),
+      Foreground = Color3.fromRGB(24, 24, 24),
+      Text = Color3.fromRGB(119, 209, 138),
+      Hover = Color3.fromRGB(24, 24, 24)
+  },
+  Synapse = {
+      Background = Color3.fromRGB(13, 15, 12),
+      Accent = Color3.fromRGB(46, 48, 43),
+      Foreground = Color3.fromRGB(36, 38, 35),
+      Text = Color3.fromRGB(152, 99, 53),
+      Hover = Color3.fromRGB(24, 24, 24)
+  },
+  Serpent = {
+      Background = Color3.fromRGB(31, 41, 43),
+      Accent = Color3.fromRGB(0, 166, 58),
+      Foreground = Color3.fromRGB(22, 29, 31),
+      Text = Color3.fromRGB(255, 255, 255),
+      Hover = Color3.fromRGB(22, 29, 31)
+  }
+}
+print("[GITHUB]: Successfully returned themes")
